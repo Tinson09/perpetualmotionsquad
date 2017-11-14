83d00ca754c1d70040cfbd85c72e037c00a84c32
Aasif Faizal <aasiffaizal.mec@gmail.com>
2017-11-14 20:22:34 +0530

    install server

---
+++ install.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt install python
+sudo apt install python-setuptools python-pip
+sudo -H pip install --upgrade pip
+sudo -H pip install pyotp
+sudo -H pip install --upgrade django 
